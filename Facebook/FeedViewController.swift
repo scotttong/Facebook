427a33be7e549ffab89d2d19b7a9352e2d55ebce
scotttong <scotttong@sockmonster-2.local>
2015-02-25 23:19:19 -0800

    took over the modal transition

--- Facebook/FeedViewController.swift
+++ Facebook/FeedViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+	
+	var isPresenting: Bool = true
 
     @IBOutlet weak var feedImageView: UIImageView!
     @IBOutlet weak var feedScrollView: UIScrollView!
@@ -27,6 +29,51 @@ class FeedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 	
+	func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+		isPresenting = true
+		return self
+	}
+	
+	func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+		isPresenting = false
+		return self
+	}
+	
+	func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+		// The value here should be the duration of the animations scheduled in the animationTransition method
+		return 0.4
+	}
+	
+	func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+		println("animating transition")
+		var containerView = transitionContext.containerView()
+		var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+		var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+		
+		if (isPresenting) {
+			containerView.addSubview(toViewController.view)
+			toViewController.view.alpha = 0
+			UIView.animateWithDuration(0.4, animations: { () -> Void in
+				toViewController.view.alpha = 1
+				}) { (finished: Bool) -> Void in
+					transitionContext.completeTransition(true)
+			}
+		} else {
+			UIView.animateWithDuration(0.4, animations: { () -> Void in
+				fromViewController.view.alpha = 0
+				}) { (finished: Bool) -> Void in
+					transitionContext.completeTransition(true)
+					fromViewController.view.removeFromSuperview()
+			}
+		}
+	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+		var destinationVC = segue.destinationViewController as UIViewController
+		destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
+		destinationVC.transitioningDelegate = self
+		
+	}
 	
 	@IBAction func onTapThumbnail(sender: AnyObject) {
 		performSegueWithIdentifier("photoSegue", sender: self)
